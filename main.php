11b2b3c44114414abad89c44dcf9efa3efc68031
root <root@ip-172-31-36-172.ap-southeast-2.compute.internal>
2016-09-14 22:39:38 +1000

    initial theme assets.

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Rotary Ride For Research Dollars</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+
+<h1> Test Title </h1>
+
+
+</body>
+
+</html>

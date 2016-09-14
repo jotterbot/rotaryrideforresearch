55bbdd110a6d41d66121e02a8d50ae940623a2f2
James Otter <jotterbot@gmail.com>
2016-09-14 23:58:45 +1000

    super ghetto bad tinkering

--- main.php
+++ main.php
@@ -4,12 +4,73 @@
 <head>
     <title>Rotary Ride For Research Dollars</title>
     <meta charset="utf-8" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
 
 <body>
 
-<h1> Test Title </h1>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	<a class="navbar-brand" href="#"><span>
+        <img alt="Rotary Ride For Research Dollars" src="wp-content/themes/rotaryrideforresearch/images/rotary-logo.png" style="height:25px;">
+        Rotary Ride For Research Dollars
+</span>
+      </a>
+    </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+     <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://australianrotaryhealth.org.au/">Australian Rotary Health</a></li>
+        <li><a href="#">The Ride</a></li>
+
+	<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<!--
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="wp-content/themes/rotaryrideforresearch/images/rotary-logo.png" style="height:50px;">
+      </a>
+    </div>
+  </div>
+</nav>
+-->
+
+<div class="jumbotron">
+  <div class="container">
+	<h3>Welcome!</h3>
+	<div style="background-image: url('http://www.rotaryrideforresearch.com.au/images/home_1_00.png');background-size: cover;background-repeat: no-repeat;background-position: center center;"></div>
+  </div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </body>
 
